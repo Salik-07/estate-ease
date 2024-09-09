dfc74dbf6f4796dbb266f2abad942cccdb3e262d
Salik-07 <salikhussain41@gmail.com>
2024-09-09 15:22:14 +0500

    Add User and Prisma modules

---
+++ src/user/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface SignupParams {
+  name: string;
+  phone: string;
+  email: string;
+  password: string;
+}
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async signup({ email }: SignupParams) {
+    const userExists = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userExists) {
+      throw new ConflictException();
+    }
+  }
+}
